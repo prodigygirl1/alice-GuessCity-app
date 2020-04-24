ec5a5a3a4cdc375006b3eec54cd7125a94e753e7
prodigygirl1 <Prodigygirl1.0@yandex.ru>
2020-04-24 22:34:51 +0400

    add guess_country 1

--- main.py
+++ main.py
@@ -224,7 +224,7 @@ def get_first_name(req):
 def get_country(city):
     url = "https://geocode-maps.yandex.ru/1.x/"
     params = {
-        'geocode': city,
+        'geocode': city.title(),
         'format': 'json'
     }
 
